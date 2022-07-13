e7aee95d60b1b4699d499b8d131537d39994fd0e
rteitch <rizaltaufiq85@gmail.com>
2022-07-13 11:33:46 +0700

    ubah migrasi order r s

--- database/migrations/2022_06_15_225343_create_order_r_s_table.php
+++ database/migrations/2022_06_15_225343_create_order_r_s_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->enum('status', ['PROSES', 'DITERIMA']);
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
